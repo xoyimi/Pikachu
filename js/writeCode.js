d0f346f2fdf42ee8f5a7b15f8e0365110556a477
xoyimi-notebook <xoyimi@foxmail.com>
2020-12-23 18:08:48 +0800

    封装Promise函数

--- js/writeCode.js
+++ js/writeCode.js
@@ -4,28 +4,36 @@
  * @param {number}  speed 控制速度，单位ms
  *
  * */
+
+
+
+
 export function writeCode(element, code, speed = 50) {
-  let n = 0;
-  let styleTag = document.createElement('style');
-  document.querySelector('head').append(styleTag)
+  return new Promise(resolve => {
+    let n = 0;
+    let styleTag = document.createElement('style');
+    document.querySelector('head').append(styleTag)
 
-  //代码速度控制 使用时间代理
-  document.querySelector('.btn-wrapper').addEventListener('click', (e) => {
-    if (e.target['nodeName'] === 'BUTTON') {
-      speed=Number(e.target['dataset']['speed'])
-    }
-  })
-
-  ;(function helper() {
-    setTimeout(() => {
-      if (n < code.length) {
-        element.scrollTo(0, element.scrollHeight)
-        element.innerHTML = Prism.highlight(code.slice(0, n), Prism.languages.css, 'css')
-        styleTag.innerHTML += code[n]
-        n++
-        setTimeout(helper, speed)
+    //代码速度控制 使用时间代理
+    document.querySelector('.btn-wrapper').addEventListener('click', (e) => {
+      if (e.target['nodeName'] === 'BUTTON') {
+        speed = Number(e.target['dataset']['speed'])
       }
+    })
 
-    }, speed)
-  })()
+    ;(function helper() {
+      setTimeout(() => {
+        if (n < code.length) {
+          element.scrollTo(0, element.scrollHeight)
+          element.innerHTML = Prism.highlight(code.slice(0, n), Prism.languages.css, 'css')
+          styleTag.innerHTML += code[n]
+          n++
+          setTimeout(helper, speed)
+        } else {
+          resolve()
+        }
+
+      }, speed)
+    })()
+  })
 }
